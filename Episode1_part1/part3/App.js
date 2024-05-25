f6ecdaca4b671e0708ee442079f0598632a81b01
Radhika Joshi <radhika7002@gmail.com>
2024-05-25 15:17:28 -0700

    Episode 1

---
+++ Episode1_part1/part3/App.js
@@ -0,0 +1,7 @@
+const headingfromReact = React.createElement(
+  "h1",
+  { id: "heading" }, //attributes to tag can be given here id ="heading", xyz = "abc"
+  "Hello World from React!"
+);
+const root_react = ReactDOM.createRoot(document.getElementById("root"));
+root_react.render(headingfromReact);
