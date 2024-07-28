73bf418d9fb999078d059175835793a535ef3715
Radhika Joshi <radhika7002@gmail.com>
2024-07-28 06:44:45 -0700

    Episode3

--- Episode2/index.js
+++ Episode2/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const headingfromReact = React.createElement(
-  "h1",
-  { id: "heading" }, //attributes to tag can be given here id ="heading", xyz = "abc"
-  "Hello World from React! 🚀"
-);
+// const headingfromReact = React.createElement(
+//   "h1",
+//   { id: "heading" }, //attributes to tag can be given here id ="heading", xyz = "abc"
+//   "Hello World from React! 🚀"
+// );
+
+//Episode 3
+const jsxHeading = <h1>Hello World from JSX🚀</h1>;
 const root_react = ReactDOM.createRoot(document.getElementById("root"));
-root_react.render(headingfromReact);
+//root_react.render(headingfromReact);
+root_react.render(jsxHeading);
